ed3e1e2eb9b4903320f0c9a34781867cc274884e
thomasrpo <thomasrr29@gmail.com>
2024-07-27 15:03:19 -0500

    Entities created

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,28 @@
+import { User } from 'src/users/entities/user.entity'
+import {Column, PrimaryGeneratedColumn, ManyToMany, Entity, JoinTable} from 'typeorm'
+
+@Entity()
+export class Event {
+
+    @PrimaryGeneratedColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    hour: Date
+
+    @ManyToMany(() => User)
+    @JoinTable()
+    users: string
+
+    @Column()
+    person: string
+
+    @Column()
+    ubication: String
+
+    @Column()
+    precio: string
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {PrimaryGeneratedColumn, Column, Entity, ManyToMany, JoinTable} from 'typeorm'
+import { Event } from 'src/events/entities/event.entity'
+
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    last_name: string
+
+    @Column()
+    cellphone: string
+
+    @Column()
+    email: string
+
+    @ManyToMany(() => Event)
+    @JoinTable()
+    events: string
+    
+    
+}
